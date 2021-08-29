9aeff35dacd256e6575aabcbf5a4f2d501ce191e
Giardino <infected_giardinno@hotmail.com>
2021-08-28 23:05:56 -0500

    first commit

---
+++ src/main/java/com/trackr/challenge/service/Hello.java
@@ -0,0 +1,18 @@
+package com.trackr.challenge.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class Hello {
+
+    @GET
+    @Path("{var:.*}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello World";
+    }
+    
+}
